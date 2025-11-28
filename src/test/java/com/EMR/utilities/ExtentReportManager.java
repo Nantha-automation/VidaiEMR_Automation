6747a71e43de688651bcae87e2ba802ede8b33ac
nanthakumar <nanthakumar1129@gmail.com>
2025-11-28 18:54:29 +0530

    Added Suppress warnings for unused imports in the ExtentReportManager.java file

--- src/test/java/com/EMR/utilities/ExtentReportManager.java
+++ src/test/java/com/EMR/utilities/ExtentReportManager.java
@@ -14,6 +14,7 @@ import java.util.Date;
  * ExtentReports Manager for generating detailed HTML test reports with screenshots
  * Supports step-by-step test execution reporting
  */
+@SuppressWarnings("unused")
 public class ExtentReportManager {
 
     private static ExtentReports extentReports;
