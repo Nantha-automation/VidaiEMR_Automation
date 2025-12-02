7d537c9aed0630496ff91db100c9274bdc9e58a8
nanthakumar <nanthakumar1129@gmail.com>
2025-12-02 11:16:03 +0530

    Adding the CukesRunner.java file which got deleted

---
+++ src/test/java/com/EMR/runners/CukesRunner.java
@@ -0,0 +1,22 @@
+package com.EMR.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "pretty",
+                "html:target/default-html-reports.html",
+                "json:target/cucumber.json",
+                "rerun:target/rerun.txt",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
+        },
+        features = "src/test/resources/features",
+        glue = "com.EMR.stepDefinitions",
+        dryRun = false,
+        tags = "@regression"
+)
+public class CukesRunner {
+}
